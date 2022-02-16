76b5e54f4e4cabb900a9768fbe4d65987b641968
= <patrickisgreene@protonmail.com>
2022-02-16 14:56:59 -0600

    Call SkyBox::ready method before rendering

--- src/systems/render_surface.rs
+++ src/systems/render_surface.rs
@@ -12,7 +12,7 @@ pub fn render_surface(
     base_render_graph: Res<BaseRenderGraph>,
     pbr_routine: Res<PbrRoutine>,
     windows: Res<Windows>,
-    skyboxes: Res<SkyBoxes>,
+    mut skyboxes: ResMut<SkyBoxes>,
     ambient: Res<AmbientLight>
 ) {
     for (id, Surface { surface, tone_mapping, .. }) in surfaces.surfaces.iter() {
@@ -26,11 +26,18 @@ pub fn render_surface(
         // Build a rendergraph
         let mut graph = rend3::graph::RenderGraph::new();
 
+        let skybox = if let Some(skybox) = skyboxes.sky_boxes.get_mut(id) {
+            skybox.routine.ready(&renderer.0);
+            Some(&skybox.routine)
+        } else {
+            None
+        };
+
         base_render_graph.add_to_graph(
             &mut graph,
             &ready,
             &pbr_routine,
-            skyboxes.sky_boxes.get(id).map(|x|&x.routine),
+            skybox,
             &tone_mapping,
             UVec2::new(window.width() as u32, window.height() as u32),
             rend3::types::SampleCount::One,
